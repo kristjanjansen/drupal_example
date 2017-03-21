97311aeec478e69d69d993a0df5c62b73a16d261
kristjanjansen <kristjan.jansen@gmail.com>
2017-03-21 22:22:23 +0200

    cleanup

--- themes/custom/combo/src/main.js
+++ themes/custom/combo/src/main.js
@@ -1,24 +1,12 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+
 import axios from 'axios';
 
-import FrontPage from './pages/FrontPage.vue'
-import ArticlePage from './pages/ArticlePage.vue'
-
-Vue.use(VueRouter)
-
-const routes = [
-  { path: '/', component: FrontPage },
-  { path: '/article/:id', component: ArticlePage },
-]
-
-const router = new VueRouter({ routes, mode: 'history' })
-
 var http = axios.create();
-
 Vue.prototype.$http = http
 
-var events = new Vue()
-Vue.prototype.$events = events
+import VueRouter from 'vue-router'
+import router from './router';
 
+Vue.use(VueRouter)
 new Vue({ router }).$mount('#app')
