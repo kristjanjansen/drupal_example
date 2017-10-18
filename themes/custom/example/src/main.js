fa0c06c97314bd5f15735e87b738a7b195082d1b
kristjanjansen <kristjan.jansen@gmail.com>
2017-10-18 16:29:09 +0300

    new router

--- themes/custom/example/src/main.js
+++ themes/custom/example/src/main.js
@@ -2,39 +2,17 @@ import Frontpage from './pages/Frontpage.js'
 import Articlepage from './pages/Articlepage.js'
 import Userpage from './pages/Userpage.js'
 
+const routes = {
+    '/': Frontpage,
+    '/article': Articlepage,
+    '/user': Userpage,       
+}
+
 const route = window.location.pathname.split('/')
 
-if (route[1] === 'article') {
-    Articlepage(route[2]).then(html => {
-        document.querySelector('#app').innerHTML = html
-    })    
-}
-else if (route[1] === 'user') {
-    Userpage(route[2]).then(html => {
-        document.querySelector('#app').innerHTML = html
-    })    
-} else {
-    Frontpage().then(html => {
-        document.querySelector('#app').innerHTML = html
-    })    
+const render = async () => {
+    const html = await routes[`/${route[1]}`](route[2])
+    document.querySelector('#app').innerHTML = html
 }
 
-//getArticles().then(articles =>
-    //document.querySelector('#app').innerHTML = articles.map(Article).join('')
-//)
-/*
-const route = window.location.pathname.split('/')
-
-console.log(route)
-
-if (route[1] === 'article') {
-    getArticleWithComments(route[2]).then(article =>
-        document.querySelector('#app').innerHTML = Article(article) + article.comments.map(Comment).join('')
-    )
-} else {
-    getArticles().then(articles =>
-        document.querySelector('#app').innerHTML = articles.map(Article).join('')
-    )
-}
-*/
-
+render()
