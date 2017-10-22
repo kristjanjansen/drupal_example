f02405a7497b325984e91021f348f9c03d6703a3
kristjanjansen <kristjan.jansen@gmail.com>
2017-10-22 22:35:07 +0300

    fixes

--- themes/custom/example_vue/src/components/Header2.js
+++ themes/custom/example_vue/src/components/Header2.js
@@ -2,7 +2,7 @@ export default {
     template: `
         <header>
             <a href="/">
-                <h1>DrupalCamp Baltic demo site</h1>
+                <h1>Vue demo site</h1>
             </a>
         </header>
     `
