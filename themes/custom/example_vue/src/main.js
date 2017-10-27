a4373a6df23bd7bec8f612e1d668ed39c7bf57e6
kristjanjansen <kristjan.jansen@gmail.com>
2017-10-27 06:53:56 +0300

    rm comment

--- themes/custom/example_vue/src/main.js
+++ themes/custom/example_vue/src/main.js
@@ -19,31 +19,3 @@ new Vue({
     }
 })
 
-/*
-new Vue({
-    el: '#app',
-    components: { Userpage },
-    template: `
-        <userpage uuid="41ea5ed7-5421-4e3a-9bf6-df3889c998c4"></userpage>
-    `
-})
-*/
-/*
-
-        <articlepage uuid="73942b4f-41e2-4877-bb67-f3dbf28eb714"></articlepage>
-
-const routes = {
-    '/': Frontpage,
-    '/article': Articlepage,
-    '/user': Userpage,       
-}
-
-const route = window.location.pathname.split('/')
-
-const render = async () => {
-    const html = await routes[`/${route[1]}`](route[2])
-    document.querySelector('#app').innerHTML = html
-}
-
-render()
-*/
